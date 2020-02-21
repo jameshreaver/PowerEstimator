03327113d852e6de74663d31032d06ae7d6e0056
jameshreaver <10749703+jameshreaver@users.noreply.github.com>
2020-02-21 11:38:33 +0000

    Handle input/output into simulation

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
+const readline = require('readline');
 const Simulation = require('./src/Simulation');
 
 
-let lines = [
-  "1544206562 TurnOff",
-  "1544206563 Delta +0.5",
-  "1544210163 Delta -0.25",
-  "1544211963 Delta +0.75",
-  "1544211963 Delta +0.75",
-  "1544213763 TurnOff"
-];
+let rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-let simulation = new Simulation(5);
-simulation.run(lines);
-simulation.print();
+let lines = [];
+
+
+rl.on('line', function(line){
+    lines.push(line);
+})
+
+rl.on('close', function() {
+    let simulation = new Simulation(5);
+    simulation.run(lines);
+    simulation.print();
+});

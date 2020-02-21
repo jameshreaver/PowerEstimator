990da6a119e40a20e2c5ea7a5dbc5a623ceb63c5
jameshreaver <10749703+jameshreaver@users.noreply.github.com>
2020-02-21 11:28:46 +0000

    Implement power consumption calculation logic

--- src/PowerEstimator.js
+++ src/PowerEstimator.js
@@ -1,11 +1,30 @@
+const Lamp = require('./Lamp');
+
 class PowerEstimator {
-  
-  constructor() {
+
+  constructor(power) {
+    this.lamp = new Lamp();
+    this.power = power;
     this.estimate = 0;
   }
 
+  // Processes ordered messages
   process(messages) {
-    return;
+    messages.forEach((message) => {
+      this.processOne(message);
+    });
+  }
+
+  processOne(message) {
+    let hours = this.computeHours(message);
+    this.estimate += hours * this.power * this.lamp.brightness;
+    this.timestamp = message.timestamp;
+    this.lamp.adjust(message.delta);
+  }
+
+  computeHours(message) {
+    if (this.timestamp === undefined) return 0;
+    return (message.timestamp - this.timestamp)/3600;
   }
   
 }

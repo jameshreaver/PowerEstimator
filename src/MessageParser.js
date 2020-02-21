8e4bfb7fa21c1a2b329245e7f2dd45c19f25d766
jameshreaver <10749703+jameshreaver@users.noreply.github.com>
2020-02-21 10:56:35 +0000

    Implement expected behaviour for MessageParser

--- src/MessageParser.js
+++ src/MessageParser.js
@@ -1,8 +1,20 @@
+const Message = require('./Message');
+
 class MessageParser {
 
   // Parses lines to Message objects
   parse(lines) {
-    return []
+    return lines.map((line) => {
+      return this.parseOne(line);
+    });
+  }
+
+  parseOne(line) {
+    let parts = line.split(' ');
+    let timestamp = parseInt(parts[0]);
+    let hasDelta = parts[1] === 'Delta';
+    let delta = hasDelta ? parseFloat(parts[2]) : -1;
+    return new Message(timestamp, delta);
   }
 
 }
--- test/testMessageParser.js
+++ test/testMessageParser.js
@@ -9,28 +9,28 @@ describe('MessageParser', function() {
 
   it('should parse TurnOff message', function() {
     let line = timestamp + " TurnOff";
-    let message = parser.parse([line]);
+    let message = parser.parse([line])[0];
     assert.equal(message.timestamp, timestamp);
     assert.equal(message.delta, -1);
   });
 
   it('should parse positive Delta message', function() {
     let line = timestamp + " Delta +0.5";
-    let message = parser.parse([line]);
+    let message = parser.parse([line])[0];
     assert.equal(message.timestamp, timestamp);
     assert.equal(message.delta, 0.5);
   });
 
   it('should parse negative Delta message', function() {
     let line = timestamp + " Delta -0.75";
-    let message = parser.parse([line]);
+    let message = parser.parse([line])[0];
     assert.equal(message.timestamp, timestamp);
     assert.equal(message.delta, -0.75);
   });
 
   it('should parse TurnOff message with value', function() {
     let line = timestamp + " TurnOff 0.75";
-    let message = parser.parse([line]);
+    let message = parser.parse([line])[0];
     assert.equal(message.timestamp, timestamp);
     assert.equal(message.delta, -1);
   });
